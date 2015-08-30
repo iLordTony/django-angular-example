0ec6cd2839b46e2db5ad78ec6c186f6a4ecd4d93
Carlos Tonatihu <carlostonatihu@gmail.com>
2015-08-29 19:26:12 -0500

    Creo que esta es la mejor forma para los modelos

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import MyUser, Post, Photo
+from .models import Profile, Post, Photo
 
 # Register your models here.
-admin.site.register(MyUser)
+admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(Photo)
--- api/models.py
+++ api/models.py
@@ -4,17 +4,26 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class MyUser(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User)
-    followers = models.ManyToManyField(User, related_name='followees', symmetrical=False)
+    followers = models.ManyToManyField('self', symmetrical=False, blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Post(models.Model):
-    author = models.ForeignKey(User, related_name='posts')
+    author = models.ForeignKey(User)
     title = models.CharField(max_length=255)
     body = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Photo(models.Model):
-    post = models.ForeignKey(Post, related_name='photos')
+    post = models.ForeignKey(Post)
     image = models.ImageField(upload_to="%Y/%m/%d")
+
+    def __str__(self):
+        return self.post.title
